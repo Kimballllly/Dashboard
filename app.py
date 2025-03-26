35a23d2eea31dcfce274db73611ec521f57b35e3
Kimballllly <justinejade.sulo@cvsu.edu.ph>
2025-03-26 21:39:31 +0800

    Changes in port

--- app.py
+++ app.py
@@ -516,14 +516,9 @@ def require_login():
         return redirect(url_for('login'))
 
 
-<<<<<<< HEAD
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
-=======
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
 
 
 
 
->>>>>>> 2dc7fba (hehe)
